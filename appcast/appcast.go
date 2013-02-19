ef0952070851b89b56e66b8cf4ffdf664c26ed5b
c9s <cornelius.howl@gmail.com>
2013-02-19 21:47:10 +0800

    remove Date related stuff, since its in rss package

--- appcast/appcast.go
+++ appcast/appcast.go
@@ -4,34 +4,7 @@ import (
     "encoding/xml"
     "io/ioutil"
     "net/http"
-    "time"
 )
-
-type Date string
-
-func (self Date) Parse() (time.Time, error) {
-    t, err := time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", string(self)) // Wordpress format
-    if err != nil {
-        t, err = time.Parse(time.RFC822, string(self)) // RSS 2.0 spec
-    }
-    return t, err
-}
-
-func (self Date) Format(format string) (string, error) {
-    t, err := self.Parse()
-    if err != nil {
-        return "", err
-    }
-    return t.Format(format), nil
-}
-
-func (self Date) MustFormat(format string) string {
-    s, err := self.Format(format)
-    if err != nil {
-        return err.Error()
-    }
-    return s
-}
 
 func ParseContent(text []byte) (*RSS, error) {
     var rss = RSS{}
