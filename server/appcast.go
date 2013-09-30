73e124c4037655d44cfaa43af4e189b3c546d993
c9s <cornelius.howl@gmail.com>
2013-09-30 11:08:09 +0800

    Move appcast.xml handler out

--- server/appcast.go
+++ server/appcast.go
@@ -254,41 +254,6 @@ func ScanRowToAppcastItem(rows *sql.Rows) (*appcast.Item, error) {
 	return &item, nil
 }
 
-func AppcastXmlHandler(w http.ResponseWriter, r *http.Request) {
-	var channelRegExp = regexp.MustCompile("/appcast/([^/]+)/([^/]+)")
-	var submatches = channelRegExp.FindStringSubmatch(r.URL.Path)
-	var channelIdentity = submatches[1]
-	var channelToken = submatches[2]
-
-	log.Println(r.URL)
-
-	if channel := FindChannelByIdentity(channelIdentity, channelToken); channel != nil {
-		w.Header().Set("Content-Type", "text/xml; charset=UTF-8")
-
-		rows, err := QueryReleasesByChannel(channelIdentity)
-		if err != nil {
-			log.Fatal("Query failed:", err)
-		}
-		defer rows.Close()
-
-		appcastRss := appcast.New()
-		appcastRss.Channel.Title = channel.Title
-		appcastRss.Channel.Description = channel.Description
-		appcastRss.Channel.Link = "http://" + r.Host + "/appcast/" + channelIdentity + ".xml"
-		// appcastRss.Channel.Language = channel.Language
-
-		for rows.Next() {
-			if item, err := ScanRowToAppcastItem(rows); err == nil {
-				appcastRss.Channel.AddItem(item)
-			}
-		}
-		appcastRss.WriteTo(w)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Channel not found"))
-	}
-}
-
 /*
 For route: /download/gotray/{token}
 
---
+++ server/handler_appcast.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/c9s/appcast"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func AppcastXmlHandler(w http.ResponseWriter, r *http.Request) {
+	var channelRegExp = regexp.MustCompile("/appcast/([^/]+)/([^/]+)")
+	var submatches = channelRegExp.FindStringSubmatch(r.URL.Path)
+	var channelIdentity = submatches[1]
+	var channelToken = submatches[2]
+
+	log.Println(r.URL)
+
+	if channel := FindChannelByIdentity(channelIdentity, channelToken); channel != nil {
+		w.Header().Set("Content-Type", "text/xml; charset=UTF-8")
+
+		rows, err := QueryReleasesByChannel(channelIdentity)
+		if err != nil {
+			log.Fatal("Query failed:", err)
+		}
+		defer rows.Close()
+
+		appcastRss := appcast.New()
+		appcastRss.Channel.Title = channel.Title
+		appcastRss.Channel.Description = channel.Description
+		appcastRss.Channel.Link = "http://" + r.Host + "/appcast/" + channelIdentity + ".xml"
+		// appcastRss.Channel.Language = channel.Language
+
+		for rows.Next() {
+			if item, err := ScanRowToAppcastItem(rows); err == nil {
+				appcastRss.Channel.AddItem(item)
+			}
+		}
+		appcastRss.WriteTo(w)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Channel not found"))
+	}
+}
